0d8ff0e13b0df5fca0d933547cb47861f26b0a9b
Saravana0699 <saravanatamilan06@gmail.com>
2024-02-02 20:08:12 +0530

    java file added

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5, 8};
+        int[] num = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            num[i] = arr[i];
+            System.out.println(num[i]);
+        }
+}
+}
